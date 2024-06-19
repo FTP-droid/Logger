cb9640cac0c662d68e65b1bd8748d68ad08b5e20
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2024-06-19 16:14:49 -0400

    Added default logger class with default constructer, ERR, and DEBUG methods for testing. Tested the ERR method with a generic message.

--- main/index.ts
+++ main/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+class Logger {
+    logAll;
+    constructor(){
+        this.logAll = true;
+    }
+
+    static ERR(errMsg: string){
+        console.log(errMsg);
+    }
+
+    static DEBUG(dbgMsg: string){
+        console.log(dbgMsg);
+    }
+
+
+}
+
+Logger.ERR("There was an error!");
