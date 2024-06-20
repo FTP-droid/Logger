41bf373948af31b43749102086ba1f85b4ba4ac1
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2024-06-20 00:38:20 -0400

    Migrated logger class from 'index.ts' to new file 'logger.ts'.

---
+++ main/logger.ts
@@ -0,0 +1,69 @@
+import path from 'node:path';
+
+type logLevel = 'logAll' | 'error' | 'debug' | 'warn' | 'info' | 'none';
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+class Logger {
+    logLevel: logLevel;
+    verbose: boolean;
+
+    constructor(){
+        this.logLevel = 'logAll';
+        this.verbose = false;
+    }
+
+    getLogLevel(){
+        console.log(`Log Level: ${this.logLevel}`);
+    }
+
+    setLogLevel(newLogLevel: logLevel){
+        if(newLogLevel !== 'logAll'  && newLogLevel !== 'error'  && newLogLevel !== 'debug'  
+            && newLogLevel !== 'warn' && newLogLevel !== 'info' && newLogLevel !== 'none'){
+            console.log(`Log level not updated. INVALID INPUT: ${newLogLevel}.\n The logger will continue logging with previous log evel: ${this.logLevel}`);
+            return;
+        }
+
+        this.logLevel = newLogLevel;
+    }
+
+    getVerbose(){
+        console.log(this.verbose);
+    }
+
+    setVerbose(newVerbose: boolean){
+        this.verbose = newVerbose;
+    }
+
+    ERROR(errMsg: string){
+        if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'error')){
+            return;
+        }
+        console.log(`There was an error with message: ${errMsg}`);
+    }
+
+    DEBUG(dbgMsg: string){
+        if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'debug')){
+            return;
+        }
+        console.log(`Debug message: ${dbgMsg}`);
+    }
+
+    WARN(warnMsg: string){
+        if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'error')){
+            return;
+        }
+        console.log(`Warning: ${warnMsg}`);
+    }
+
+    INFO(infoMSG: string){
+        if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'info')){
+            return;
+        }
+
+        console.log(`Info: ${infoMSG}`);
+    }
+}
+
+export default Logger;
