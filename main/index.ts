bf89b126b386b2560659708232843e39f6270c72
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2024-06-19 20:00:42 -0400

    Added guard clauses for each logging method in the Logger class. These clauses ensure that the function exits earlier if the required logLevel to output code from that function is not set.

--- main/index.ts
+++ main/index.ts
@@ -4,7 +4,7 @@ class Logger {
     logLevel: logLevel;
 
     constructor(){
-        this. logLevel = 'logAll';
+        this.logLevel = 'logAll';
     }
 
     getLogLevel(){
@@ -14,7 +14,7 @@ class Logger {
     setLogLevel(newLogLevel: logLevel){
         if(newLogLevel !== 'logAll'  && newLogLevel !== 'error'  && newLogLevel !== 'debug'  
             && newLogLevel !== 'warn' && newLogLevel !== 'info' && newLogLevel !== 'none'){
-            console.log(`Log level not updated. INVALID INPUT: ${newLogLevel}.\n The logger will continue logging with previous logLevel ${this.logLevel}`);
+            console.log(`Log level not updated. INVALID INPUT: ${newLogLevel}.\n The logger will continue logging with previous log evel: ${this.logLevel}`);
             return;
         }
 
@@ -22,23 +22,34 @@ class Logger {
     }
 
     ERROR(errMsg: string){
+        if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'error')){
+            return;
+        }
         console.log(`There was an error with message: ${errMsg}`);
     }
 
     DEBUG(dbgMsg: string){
+        if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'debug')){
+            return;
+        }
         console.log(`Debug message: ${dbgMsg}`);
     }
 
     WARN(warnMsg: string){
+        if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'error')){
+            return;
+        }
         console.log(`Warning: ${warnMsg}`);
     }
 
     INFO(infoMSG: string){
+        if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'info')){
+            return;
+        }
         console.log(`Info: ${infoMSG}`);
     }
 }
 
 const logger = new Logger();
-logger.setLogLevel('logAll');
 logger.getLogLevel();
 logger.INFO('Updated!');
