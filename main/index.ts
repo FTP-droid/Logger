ee9589e571bed944d6dd2a43dc407c06c213c0ac
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2024-06-21 01:59:43 -0400

    Updated the logger class to handle multiple timers running at the same time. TIMER_END->TIMER_STOP, TIMER_STOP function logs the current elapsed time and stops the timer or lets it continue based on the 'log' parameter. Tested timer functionality in 'index.ts'.

--- main/index.ts
+++ main/index.ts
@@ -4,9 +4,18 @@ let logger = new Logger('First Logger');
 let logger2 = new Logger('Second Logger');
 Logger.getLoggerCount();
 logger.TIMER_START("timer 1");
+logger.TIMER_START("timer 1");
+logger.TIMER_START("timer 2");
+logger2.TIMER_START("timer 2");
 
 setTimeout(() => {
-    logger.TIMER_END("timer 1");
+    logger.TIMER_STOP("timer 1");
+    logger.TIMER_STOP("timer 2", true);
+    setTimeout(() => {
+        logger2.TIMER_STOP("timer 2");
+        logger.TIMER_STOP("timer 2");
+    }, 1000);
 }, 1000);
+
 
 logger.INFO("Invalid input!");
--- main/logger.ts
+++ main/logger.ts
@@ -11,8 +11,7 @@ class Logger {
     private loggerName: string;
     private logLevel: logLevel;
     private globalOutputDateAndTime = false;
-    private timerStarted = false;
-    private timerStartValue = 0;
+    private timers: Map<string, number> = new Map();
 
     private static loggers: Map<string, Logger> = new Map();
 
@@ -30,17 +29,16 @@ class Logger {
         console.log(Logger.loggers.size);
     }
 
-    //Get the logger name
     getLoggerName(){
         console.log(`The name of this logger is: ${this.loggerName}`)
     }
     
-    //Get the current log level.
     getLogLevel(){
         console.log(`Log Level: ${this.logLevel}`);
     }
     
-    //Sets which types of logs are output. If an invalid input is used with this function, it exits early while preserving the previous value of logLevel.
+    //Sets which types of logs are output. 
+    //If an invalid input is used with this function, it exits early while preserving the previous value of logLevel.
     setLogLevel(newLogLevel: logLevel){
         if(newLogLevel !== 'logAll'  && newLogLevel !== 'error'  && newLogLevel !== 'debug'  
             && newLogLevel !== 'warn' && newLogLevel !== 'info' && newLogLevel !== 'none'){
@@ -51,12 +49,10 @@ class Logger {
         this.logLevel = newLogLevel;
     }
 
-    //Get the state of globalOutputDateAndTime.
     getGlobalOutputDateAndTime(){
         console.log(this.globalOutputDateAndTime);
     }
     
-    //Set the globalOutputDateAndTime variable to a new value.
     setGlobalOutputDateAndTime(newGlobalOutputDateAndTime: boolean){
         //Ensure newGlobalOutputDateAndTime is read as a boolean.
         newGlobalOutputDateAndTime = !!newGlobalOutputDateAndTime;
@@ -73,7 +69,8 @@ class Logger {
         if(throwErr){
             throw new Error(errMsg);
         }
-
+        
+        //If globalOutputDateAndTime (object variable) or outputDataAndTime (function variable) is true, add the date and time to the final output.
         let msg = this.globalOutputDateAndTime || outputDateAndTime? `[${new Date().toString()}] ` : "";
         msg += `${nodeConsoleTextToCommands.FgBlue}Error:${nodeConsoleTextToCommands.Reset} ${errMsg}`;
 
@@ -85,6 +82,8 @@ class Logger {
         if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'debug')){
             return;
         }
+
+        //If globalOutputDateAndTime (object variable) or outputDataAndTime (function variable) is true, add the date and time to the final output.
         let msg = this.globalOutputDateAndTime || outputDateAndTime? `[${new Date().toString()}] ` : "";
         msg += `${nodeConsoleTextToCommands.FgBlue}Debug:${nodeConsoleTextToCommands.Reset} ${dbgMsg}`;
 
@@ -96,6 +95,8 @@ class Logger {
         if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'error')){
             return;
         }
+
+        //If globalOutputDateAndTime (object variable) or outputDataAndTime (function variable) is true, add the date and time to the final output.
         let msg = this.globalOutputDateAndTime || outputDateAndTime? `[${new Date().toString()}] ` : "";
         msg += `${nodeConsoleTextToCommands.FgYellow}Warning:${nodeConsoleTextToCommands.Reset} ${warnMsg}`;
 
@@ -107,32 +108,46 @@ class Logger {
         if(this.logLevel == 'none' || (this.logLevel !== 'logAll' && this.logLevel !== 'info')){
             return;
         }
+
+        //If globalOutputDateAndTime (object variable) or outputDataAndTime (function variable) is true, add the date and time to the final output.
         let msg = this.globalOutputDateAndTime || outputDateAndTime? `[${new Date().toString()}] ` : "";
         msg += `${nodeConsoleTextToCommands.FgBlue}Info:${nodeConsoleTextToCommands.Reset} ${infoMSG}`;
 
         console.log(msg);
     }
 
+    //Create a new named timer if a timer with name:'timerName' does not exist.
     TIMER_START(timerName: string){
-        if(this.timerStarted){
-            this.WARN(`A timer was already started using this logger. ` +  
-                `A logger can only have one timer running at a time. \nCreate a new logger if you would like to have multiple timers running across overlapping intervals.`);
+        if(this.timers.has(timerName)){
+            this.WARN(`A timer '${timerName}' already exists in '${this.loggerName}'. A new timer was not created.`);
             return;
         }
         
-        this.timerStarted = true;
-        console.log(`${nodeConsoleTextToCommands.FgGreen}Start of ${timerName}.${nodeConsoleTextToCommands.Reset}`);
-        this.timerStartValue = Date.now();
+        console.log(`${nodeConsoleTextToCommands.FgGreen}'${timerName}' started.${nodeConsoleTextToCommands.Reset}`);
+        this.timers.set(timerName, Date.now());
     }
 
-    TIMER_END(timerName: string){
-        if(!this.timerStarted){
-            this.WARN(`TIMER_END was called but no running instance of timer was found.`);
+
+    //Stop a timer (if it exists) by default and log the elapsed time. Or log the current elapsed time without stopping a timer (if it exists).
+    TIMER_STOP(timerName: string, log = false){
+        if(!this.timers.has(timerName)){
+            this.WARN(`TIMER_STOP was called but a timer '${timerName}' was not found in '${this.loggerName}'.`);
             return;
         }
 
-        console.log(`${nodeConsoleTextToCommands.FgGreen}End of ${timerName}. \nElapsed Time: ${Date.now() - this.timerStartValue}ms.${nodeConsoleTextToCommands.Reset}`);
-        this.timerStarted = false;
+        const timerStart = this.timers.get(timerName) || 0;
+        const timerEnd = Date.now();
+        const elapsedTime = timerEnd - timerStart;
+
+        const timerMsg = log? `'${timerName}' current elapsed time: ${elapsedTime}ms.`: `'${timerName}' stopped with elapsed time: ${elapsedTime}ms.`;
+
+        const msg = `${nodeConsoleTextToCommands.FgGreen}${timerMsg}${nodeConsoleTextToCommands.Reset}`
+        console.log(msg);
+
+        if(log)
+            return;
+
+        this.timers.delete(timerName);
     }
 }
 
