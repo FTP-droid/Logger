cbd28bfcbad70f4f1659cfe578bfd25f5975d61f
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2024-06-21 18:39:16 -0400

    Refactored setTimeout to use the async/await approach when testing the timers in 'index.ts'.

--- main/index.ts
+++ main/index.ts
@@ -1,18 +1,30 @@
 import Logger from "./logger.js";
 
-let logger = new Logger('First Logger');
+let logger = new Logger('First Logger', true);
 Logger.getLoggerCount();
 logger.TIMER_START("timer 1");
 logger.TIMER_START("timer 1");
 logger.TIMER_START("timer 2");
-logger.INFO("Program working as expected.");
+logger.INFO("Program working as expected.", false);
 
-setTimeout(() => {
-    logger.TIMER_STOP("timer 1");
-    logger.TIMER_STOP("timer 2", true);
-    setTimeout(() => {
-        logger.TIMER_STOP("timer 2");
-        console.log("logs");
-        console.log(logger.getLogs());
-    }, 1000);
-}, 1000);
+async function timer(){
+    await new Promise<void> (resolve => {setTimeout(() => {
+        logger.TIMER_STOP("timer 1");
+        logger.TIMER_STOP("timer 2", true);
+        resolve();
+    }, 1000);});
+
+    await new Promise<void> (resolve => {
+        setTimeout(() => {
+            logger.TIMER_STOP("timer 2");
+        }, 1000);
+        resolve();
+    });
+
+    console.log("LOGS");
+    console.log(logger.getLogs());
+    console.log("Success!");
+}
+
+timer();
+
