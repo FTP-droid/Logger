46ecc539a83dd930fef2ae12c5d8236c72e950a7
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2024-06-22 13:59:03 -0400

    Updated index.ts to showcase a more telling demo of the logger module.

--- main/index.ts
+++ main/index.ts
@@ -4,18 +4,36 @@ let logger = new Logger('First Logger');
 let logger2 = new Logger('Second Logger');
 
 console.log(Logger.getLoggers());
-logger.setLogLevel('timers');
 logger.TIMER_START("timer 1");
 
 //A timer named timer 1 already exists, so this line of code will not start a new timer.
 logger.TIMER_START("timer 1");
 
 logger.TIMER_START("timer 2");
-logger.DEBUG("Weird bug happens here.", false);
-logger.WARN("This is a warning");
-logger.ERROR("ERROR!");
-logger.INFO("HELLO");
 
+if(0.1 + 0.2 !== 0.3){
+    logger.DEBUG("Weird math bug happens here.", false);
+}
+
+logger.WARN("Do not log excessively!");
+
+//Log an error here.
+const a = 1, b = 0;
+function divide(a: number, b: number){
+    if(b === 0){
+        logger.ERROR('Denominator is equal to 0!');
+        return;
+    }
+
+    return a / b;
+}
+
+console.log(divide(a, b));
+
+//Simple informative message
+logger.INFO("The application is working as expected.");
+
+//An example to log or stop the timers previously started.
 async function timer(){
     await new Promise<void> (resolve => {setTimeout(() => {
         logger.TIMER_STOP("timer 1");
@@ -32,7 +50,7 @@ async function timer(){
 
     console.log("LOGS");
     console.log(logger.getLogs());
-    console.log("Success!");
+    logger.INFO("Success!");
 }
 
 timer();
